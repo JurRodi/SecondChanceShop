ccdac19f71d273d010a82f1ef03c3debeb9c7cb5
JurRodi <rodijurrien@gmail.com>
2022-07-20 10:14:37 +0200

    Generated seeders for users & categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = new \App\Models\Category();
+        $category1->name = 'Music';
+        $category1->save();
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new \App\Models\User();
+        $user->name = 'Test';
+        $user->email = 't@t.com';
+        $user->password = bcrypt('123456');
+        $user->save();
+    }
+}
