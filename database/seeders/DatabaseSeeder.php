e0f1d244ee5e02f98c9eea5c66349d84992e470f
JurRodi <rodijurrien@gmail.com>
2022-07-21 18:25:56 +0200

    Edited db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         \App\Models\User::factory(10)->create();
+        \App\Models\Product::factory(20)->create();
     }
 }
