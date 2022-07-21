cd100c708143e71e3c083be45799b5fe2f39cced
JurRodi <rodijurrien@gmail.com>
2022-07-21 18:24:01 +0200

    Added content from db and minor styling

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,18 @@
-<h1>Products</h1>
+@extends('layouts/app')
+
+@section('content')
+    <h1>Products</h1>
+
+    <div class="container" style="display: flex; flex-wrap:wrap;">
+    @foreach($products as $product)
+        <div class="card container-sm" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{ $product->name }}</h5>
+                <p class="card-text">{{ $product->description }}</p>
+                <a href="/products/{{ $product->id }}" class="btn btn-primary">Show</a>
+            </div>
+        </div>
+    @endforeach
+    </div>
+@endsection
