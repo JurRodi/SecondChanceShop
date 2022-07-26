83a278450864a8db3c890b2b90e4e2f4eae7cab9
JurRodi <rodijurrien@gmail.com>
2022-07-26 14:57:39 +0200

    First draft of the nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,18 @@
 </head>
 <body>
 
+    <nav>
+        <div class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">SecondChanceShop</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="form-inline my-2 my-lg-0 ">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+            <!-- user profile -->
+        </div>
+    </nav>
+
     @yield('content')
 
 </body>
