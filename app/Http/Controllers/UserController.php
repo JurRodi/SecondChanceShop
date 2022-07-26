418b939571ab4ae14c88f30ef119024fb37021c2
JurRodi <rodijurrien@gmail.com>
2022-07-26 14:52:13 +0200

    Added register, login, store &verify functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,5 +12,43 @@ class UserController extends Controller
         $user = DB::table('users')->where('id', $id)->first();
         $data['user'] = $user;
         return view('users/index', $data);
+    }
+
+    public function register(){
+        return view('users/register');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = new \App\Models\User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return redirect('/');
+    }
+
+    public function login(){
+        return view('users/login');
+    }
+
+    public function verify(Request $request){
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+        if($user){
+            if(Hash::check($request->password, $user->password)){
+                return redirect('/');
+            }
+        }
+        return redirect('/login');
     }
 }
