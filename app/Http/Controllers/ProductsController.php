d9931ebf0b6862e132b2c19e34d2259dc1bed200
JurRodi <rodijurrien@gmail.com>
2022-07-26 14:51:48 +0200

    Added create, store, edit & delete functions

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,6 +9,8 @@ class ProductsController extends Controller
 {
     public function index(){
         $products = DB::table('products')->orderBy('id', 'desc')->get();
+        // $products = DB::table('products')->orderBy('id', 'desc')->paginate(10);
+        // dd($products);
         $data['products'] = $products;
         return view('products/index', $data);
     }
@@ -16,5 +18,43 @@ class ProductsController extends Controller
     public function show(\App\Models\Product $product){
         $data['product'] = $product;
         return view('products/show', $data);
+    }
+
+    public function create(){
+        $categories = DB::table('categories')->get();
+        $data['categories'] = $categories;
+        return view('products/create', $data);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|string',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'category_id' => 'required',
+        ]);
+
+        $product = new \App\Models\Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->image = $request->image->store('images', 'public');
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        $request->flash('message', 'Product created successfully!');
+
+        return redirect('/');
+    }
+
+    public function edit(\App\Models\Product $product){
+        $data['product'] = $product;
+        return view('products/edit', $data);
+    }
+
+    public function destroy(\App\Models\Product $product){
+        $product->delete();
+        return redirect('/products');
     }
 }
