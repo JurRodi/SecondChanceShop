157370f93e22a347337dd273d28701aad68ebb9b
JurRodi <rodijurrien@gmail.com>
2022-07-26 14:55:52 +0200

    Added register page

---
+++ resources/views/users/register.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts/app')
+
+@section('content')
+    <form method="post" action="{{ url('/register') }}">
+        @csrf
+        <h2>Register</h2>
+
+        @if($errors->any())
+            @component('components/alert')
+                @slot('type')
+                    danger
+                @endslot
+                @foreach ($errors->all() as $error)
+                    {{ $error }}<br>
+                @endforeach
+            @endcomponent
+        @endif
+
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input value="{{ old('name') }}" type="text" class="form-control" name="name" id="name" placeholder="Enter name">
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input value="{{ old('email') }}" type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+        </div>
+        <div class="form-group">
+            <label for="password_confirmation">Confirm Password</label>
+            <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
+        </div>
+
+        <!-- <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="remember">
+            <label class="form-check-label" for="remember">Remember me</label>
+        </div> -->
+        <button type="submit" class="btn btn-primary">Regsiter</button>
+    </form>
+@endsection
