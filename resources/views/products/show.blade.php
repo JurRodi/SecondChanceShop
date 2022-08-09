1e67a71b3dd585c02a8452dcaa55d0d3044e37ce
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:52:45 +0200

    Added content and styling with responsive cta's

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,19 +1,42 @@
 @extends('layouts/app')
 
 @section('content')
-    <h1>{{ $product->name }}</h1>
-    <a href="/users/{{ $product->user_id }}">{{ $product->user->name }}</a>
-
-    <h3>Category</h3>
-    <p>{{ $product->category->name }}</p>
-
-    @component('components/alert')
-        @slot('type')
-            success
-        @endslot
-        @slot('slot')
-            Succes!
-        @endslot
-    @endcomponent
-
+    <div class="w-50 ms-5 mb-3 bg-light d-flex justify-content-center" style="height: 500px;"><img src="{{ asset('storage/images/'.$product->image) }}" alt="{{ $product->name }}" class="img-fluid"></div>
+    <div class="ms-5 w-50 d-flex justify-content-between">
+        <div style="width: 70%;">
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->description }}</p>
+            <p>€ {{ $product->price }}</p>
+            <h5>Category</h5>
+            <p>{{ $product->category->name }}</p>
+            <h5>Posted by</h5>
+            <div class="mb-3">
+                <img src="{{ asset('storage/avatars/'.$product->user->avatar) }}" alt="{{ $product->user->name }}" class="img-fluid rounded-circle me-2" style="width: 50px; height: 50px;">
+                <a href="/users/{{ $product->user_id }}">{{ $product->user->name }}</a>
+            </div>
+        </div>
+        @if($product->user_id == \Auth::id())
+            <div>
+                <a href="/products/edit/{{ $product->id }}" class="btn btn-primary">Edit</a>
+                <form method="POST" action="/products/destroy/{{ $product->id }}" class="d-inline">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        @else
+            <div>
+                <form method="POST" action="/products/favorite/{{ $product->id }}" class="d-inline">
+                    @method('POST')
+                    @csrf
+                    <button type="submit" class="btn btn-warning">Favorite</button>
+                </form>
+                <form method="POST" action="/products/bid/{{ $product->id }}" class="d-inline">
+                    @method('POST')
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Place bid</button>
+                </form>
+            </div>
+        @endif
+    </div>
 @endsection
