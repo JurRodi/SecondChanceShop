7e97b32907209c34f5d19f1fb8695ddf1fd3e6a9
JurRodi <rodijurrien@gmail.com>
2022-08-09 15:01:24 +0200

    Added cloudder

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use JD\Cloudder\Facades\Cloudder;
 
 class Controller extends BaseController
 {
