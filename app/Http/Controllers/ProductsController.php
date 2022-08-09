3aad636dd45b75f3195a04a1d9ed90aaab47e037
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:58:27 +0200

    Fixed store, edit and added update functions

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+use JD\Cloudder\Facades\Cloudder;
 
 class ProductsController extends Controller
 {
     public function index(){
         $products = DB::table('products')->orderBy('id', 'desc')->get();
-        // $products = DB::table('products')->orderBy('id', 'desc')->paginate(10);
-        // dd($products);
         $data['products'] = $products;
         return view('products/index', $data);
     }
@@ -39,22 +40,58 @@ class ProductsController extends Controller
         $product->name = $request->name;
         $product->price = $request->price;
         $product->description = $request->description;
-        $product->image = $request->image->store('images', 'public');
+        // $img_name = $request->file('image')->getRealPath();
+        // $product->image = $img_name;
+        // Cloudder::upload($img_name, null);
+        // $url = cloudinary()->upload($request->file('image')->getRealPath())->getSecurePath();
+        if($request->hasFile('image')){
+            $url = $request->file('image')->getClientOriginalName();
+            $request->file('image')->store('public/images');
+            $product->image = $url;
+        }
         $product->category_id = $request->category_id;
+        $product->user_id = Auth::user()->id;
         $product->save();
 
-        $request->flash('message', 'Product created successfully!');
-
-        return redirect('/');
+        $request->flash();
+        $request->session()->flash('message', 'Product created successfully!');
+        return redirect('/products/create');
     }
 
     public function edit(\App\Models\Product $product){
+        $categories = DB::table('categories')->get();
         $data['product'] = $product;
+        $data['categories'] = $categories;
         return view('products/edit', $data);
     }
 
-    public function destroy(\App\Models\Product $product){
+    public function update(Request $request, \App\Models\Product $product){
+        $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|string',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'category_id' => 'required',
+        ]);
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        if($request->hasFile('image')){
+            $url = $request->file('image')->getClientOriginalName();
+            $request->file('image')->store('public/images');
+            $product->image = $url;
+        }
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        $request->flash();
+        $request->session()->flash('message', 'Product updated successfully!');
+        return redirect('/products/edit/'.$product->id);
+    }
+
+    public function destroy($id){
+        $product = \App\Models\Product::where('id', $id)->first();
         $product->delete();
-        return redirect('/products');
+        return redirect('/');
     }
 }
