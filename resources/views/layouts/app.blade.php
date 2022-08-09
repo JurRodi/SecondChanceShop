9c4d1797a4e39cf3400953278d82718bee96a64a
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:47:40 +0200

    Added nav that's responsive to pages and users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,24 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SecondChanceShop</title>
     <!-- <link rel="icon" type="image/x-icon" href=""> -->
-    <!-- <link rel="stylesheet" href="styling/style.css"> -->
+    @livewireStyles
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-
-    <nav>
-        <div class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">SecondChanceShop</a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <form class="form-inline my-2 my-lg-0 ">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                </form>
+    
+    <nav class="mb-4 px-4 navbar-light bg-light">
+        <div class="navbar navbar-expand-lg">
+            <a class="navbar-brand ml-auto" href="/">SecondChanceShop</a>    
+            <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+                @if(url()->current() != url('/login') && url()->current() != url('/register'))
+                    <form class="form-inline my-2 my-lg-0 w-25" autocomplete="off" method="">
+                        <livewire:product-search />
+                    </form>
+                @endif
             </div>
-            <!-- user profile -->
+            @if(\Illuminate\Support\Facades\Auth::check())
+                <div class="navbar-nav ml-auto">
+                    <div class="nav-item dropdown">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ \Illuminate\Support\Facades\Auth::user()->name }}
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/users/{{ \Auth::user()->id }}">Profile</a></li>
+                            <li><a class="dropdown-item" href="/users/favorites/{{ \Auth::user()->id }}">favorites</a></li>
+                            <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            @else
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link" href="/login">Login</a>
+                    <a class="nav-link" href="/register">Register</a>
+                </div>
+            @endif
         </div>
     </nav>
 
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
+    
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"></script> -->
+    <!-- <script src="/js/app.js"></script> -->
+    <script>
+        //show dropdown items on click
+        document.querySelector('.dropdown-toggle').addEventListener('click', function() {
+            document.querySelector('.dropdown-menu').classList.toggle('show');
+        });
 
+    </script>
+    @livewireScripts
 </body>
 </html>
