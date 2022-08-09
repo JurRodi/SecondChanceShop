98d9c342e48d95c114073a0c3b4dd202a1755522
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:38:12 +0200

    Added styling and link to login

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -16,27 +16,28 @@
             @endcomponent
         @endif
 
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="name">Name</label>
             <input value="{{ old('name') }}" type="text" class="form-control" name="name" id="name" placeholder="Enter name">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="email">Email address</label>
             <input value="{{ old('email') }}" type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="password">Password</label>
             <input type="password" class="form-control" name="password" id="password" placeholder="Password">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="password_confirmation">Confirm Password</label>
             <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
         </div>
-
-        <!-- <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="remember">
-            <label class="form-check-label" for="remember">Remember me</label>
-        </div> -->
-        <button type="submit" class="btn btn-primary">Regsiter</button>
+        <div>
+            <button type="submit" class="btn btn-primary">Regsiter</button>
+        </div>
+        <div>
+            <label>Already have an account?</label>
+            <a href="/login" >Log in here</a>
+        </div>
     </form>
 @endsection
