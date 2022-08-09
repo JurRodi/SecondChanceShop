b3c89d297a65aa6ed18b21251665051b81a428ef
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:38:30 +0200

    Added styling and link to register

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -16,18 +16,20 @@
             @endcomponent
         @endif
 
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="email">Email address</label>
             <input value="{{ old('name') }}" type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
-        <div class="form-group">
+        <div class="form-group mb-2">
             <label for="password">Password</label>
             <input type="password" class="form-control" name="password" id="password" placeholder="Password">
         </div>
-        <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="remember">
-            <label class="form-check-label" for="remember">Remember me</label>
+        <div>
+            <button type="submit" class="btn btn-primary">Log in</button>
         </div>
-        <button type="submit" class="btn btn-primary">Log in</button>
+        <div>
+            <label>No account?</label>
+            <a href="/register" >Sign up here</a>
+        </div>
     </form>
 @endsection
