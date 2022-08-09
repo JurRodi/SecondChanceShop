dc65021a44b67ea0dc8e6abafdc6fb133a554a63
JurRodi <rodijurrien@gmail.com>
2022-08-09 14:50:10 +0200

    Added logout, edit and favorite functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -11,6 +12,8 @@ class UserController extends Controller
     public function index($id){
         $user = DB::table('users')->where('id', $id)->first();
         $data['user'] = $user;
+        $products = DB::table('products')->where('user_id', $id)->get();
+        $data['products'] = $products;
         return view('users/index', $data);
     }
 
@@ -46,9 +49,44 @@ class UserController extends Controller
         $user = \App\Models\User::where('email', $request->email)->first();
         if($user){
             if(Hash::check($request->password, $user->password)){
+                Auth::login($user);
                 return redirect('/');
             }
         }
         return redirect('/login');
     }
+
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+
+    public function edit(\App\Models\User $user){
+        $data['user'] = $user;
+        return view('users/edit', $data);
+    }
+
+    // update profile picture
+    public function update(Request $request, \App\Models\User $user){
+        $request->validate([
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        if($request->hasFile('image')){
+            $url = $request->file('image')->getClientOriginalName();
+            $request->file('image')->store('public/images');
+            $user->image = $url;
+        }
+        $user->save();
+        return redirect('/users/index');
+    }
+
+    public function favorites(\App\Models\User $user){
+        $data['user'] = $user;
+        $products = DB::table('products')->where('user_id', $user->id)->get();
+        $data['products'] = $products;
+        return view('users/favorites', $data);
+    }
+    
 }
