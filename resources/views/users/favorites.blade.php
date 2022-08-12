e45918fb6cc02c71637f8a43081283ef382595df
JurRodi <rodijurrien@gmail.com>
2022-08-12 16:40:22 +0200

    Error fix favorites display

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -2,21 +2,25 @@
 
 @section('content')
     <div style="display: flex; flex-wrap:wrap;">
-        @foreach($products as $product)
-            @component('components/card')
-                    @slot('image')
-                        {{ $product->image }}
-                    @endslot
-                    @slot('name')
-                        {{ $product->name }}
-                    @endslot
-                    @slot('description')
-                        {{ $product->description }}
-                    @endslot
-                    @slot('id')
-                        {{ $product->id }}
-                    @endslot
-                @endcomponent
-        @endforeach
+        @if($products->count() > 0)
+            @foreach($products as $product)
+                @component('components/card')
+                        @slot('image')
+                            {{ $product->image }}
+                        @endslot
+                        @slot('name')
+                            {{ $product->name }}
+                        @endslot
+                        @slot('description')
+                            {{ $product->description }}
+                        @endslot
+                        @slot('id')
+                            {{ $product->id }}
+                        @endslot
+                    @endcomponent
+            @endforeach
+        @else
+            <h3 class="mx-auto mt-5">No favorite products yet</h3>
+        @endif
     </div>
 @endsection
